c2318b28f3a123f98c54c35d3592c0a831592cc4
Carol Rieping <carol.rieping@gmail.com>
2023-01-30 19:13:33 -0700

    initial commit

---
+++ db/migrate/20230130235254_create_traveller.rb
@@ -0,0 +1,10 @@
+class CreateTraveller < ActiveRecord::Migration[5.2]
+  def change
+    create_table :travellers do |t|
+      t.string :name
+      t.boolean :over_18?
+      t.integer :year
+      t.timestamp
+    end
+  end
+end
